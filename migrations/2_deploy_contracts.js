fd67d2465ca980e1e1055254329b8b99ca4d668c
prashantbhatt12 <prashantbhatt12@gmail.com>
2022-04-17 21:18:33 -0400

    updated migration added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,6 @@ var Marketplace = artifacts.require("./Marketplace.sol");
 module.exports = function (deployer) {
   deployer.deploy(Marketplace, {
     from: "0xdAe895082F056ed714060Bd5D3820F4fe4a91965",
-    value: "1",
+    value: "10000000000000000000",
   });
 };
